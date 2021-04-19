a7d54c5b4b9b3307a183b09e5f27b9e809d7dbfd
Yihen <liuyihen@gmail.com>
2021-04-19 17:42:59 +0800

    solidity fix in 0.8

--- contracts/Config.sol
+++ contracts/Config.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity=0.8.0;
 
 contract Config {
   uint constant  MAX_CRYPTO_KEY_LEN=2048;
--- contracts/lib/Verifier.sol
+++ contracts/lib/Verifier.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 //pragma solidity >=0.4.24 <0.7.0;
-pragma solidity >=0.8.0;
+pragma solidity=0.8.0;
 
 library  Verifier {
     function recoverAddr(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
